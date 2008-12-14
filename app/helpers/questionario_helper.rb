2be4717139abac7c326899d9739dfac06b2394ff
sirius.ufrj <sirius.ufrj@a3151d44-c738-0410-a532-0b09692cc0d8>
2008-12-14 19:36:02 +0000

    Versão 1.0b da Plataforma T.U.I.A. Updates: 1- Criação de questionários (SMERF). 2- Desativação/Ativação de questionários. 3- Preenchimento de questionários.

---
+++ app/helpers/questionario_helper.rb
@@ -0,0 +1,2 @@
+module QuestionarioHelper
+end
---
+++ test/functional/questionario_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class QuestionarioControllerTest < ActionController::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
